5dab1c086828b7d229f1bd26027ee3228dd817b0
shaelraj <shael.dhum88@gmail.com>
2021-03-11 14:17:16 +0530

    initial code cargo-routing

---
+++ code/ddd/cargo-routing/src/main/java/com/javamonks/routing/domain/model/valueobjects/Schedule.java
@@ -0,0 +1,38 @@
+package com.javamonks.routing.domain.model.valueobjects;
+
+
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+import com.javamonks.routing.domain.model.entities.CarrierMovement;
+
+/**
+ * A Voyage schedule
+ */
+@Embeddable
+public class Schedule {
+
+    public static final Schedule EMPTY = new Schedule();
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "voyage_id")
+    private List<CarrierMovement> carrierMovements = Collections.emptyList();
+
+    public Schedule() {
+        // Nothing to initialize.
+    }
+
+    Schedule(List<CarrierMovement> carrierMovements) {
+        this.carrierMovements = carrierMovements;
+    }
+
+    public List<CarrierMovement> getCarrierMovements() {
+        return Collections.unmodifiableList(carrierMovements);
+    }
+}
